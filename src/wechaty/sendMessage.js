56b017c9b6f5b0d5e3467e69ef98a29c03afa681
lieyan <wangxuebin@bilibili.com>
2023-02-09 13:03:31 +0800

    commit

--- src/wechaty/sendMessage.js
+++ src/wechaty/sendMessage.js
@@ -1,6 +1,7 @@
 // import { getChatGPTReply as getReply } from '../chatgpt/index.js'
-import { getOpenAiReply as getReply } from '../openai/index.js'
-import { botName, roomWhiteList, aliasWhiteList } from '../../config.js'
+import {getOpenAiReply as getReply} from '../openai/index.js'
+import {aliasWhiteList, botName, roomWhiteList} from '../../config.js'
+import {getChatGPTReply} from "../chatgpt/index.js";
 
 /**
  * 默认消息发送
@@ -9,34 +10,34 @@ import { botName, roomWhiteList, aliasWhiteList } from '../../config.js'
  * @returns {Promise<void>}
  */
 export async function defaultMessage(msg, bot) {
-  const contact = msg.talker() // 发消息人
-  const receiver = msg.to() // 消息接收人
-  const content = msg.text() // 消息内容
-  const room = msg.room() // 是否是群消息
-  const roomName = (await room?.topic()) || null // 群名称
-  const alias = (await contact.alias()) || (await contact.name()) // 发消息人昵称
-  const remarkName = await contact.alias() // 备注名称
-  const name = await contact.name() // 微信名称
-  const isText = msg.type() === bot.Message.Type.Text // 消息类型是否为文本
-  const isRoom = roomWhiteList.includes(roomName) && content.includes(`${botName}`) // 是否在群聊白名单内并且艾特了机器人
-  const isAlias = aliasWhiteList.includes(remarkName) || aliasWhiteList.includes(name) // 发消息的人是否在联系人白名单内
-  const isBotSelf = botName === remarkName || botName === name // 是否是机器人自己
-  // TODO 你们可以根据自己的需求修改这里的逻辑
-  if (isText && !isBotSelf) {
-    try {
-      // 区分群聊和私聊
-      if (isRoom && room) {
-        await room.say(await getReply(content.replace(`${botName}`, '')))
-        return
-      }
-      // 私人聊天，白名单内的直接发送
-      if (isAlias && !room) {
-        await contact.say(await getReply(content))
-      }
-    } catch (e) {
-      console.error(e)
+    const contact = msg.talker() // 发消息人
+    const receiver = msg.to() // 消息接收人
+    const content = msg.text() // 消息内容
+    const room = msg.room() // 是否是群消息
+    const roomName = (await room?.topic()) || null // 群名称
+    const alias = (await contact.alias()) || (await contact.name()) // 发消息人昵称
+    const remarkName = await contact.alias() // 备注名称
+    const name = await contact.name() // 微信名称
+    const isText = msg.type() === bot.Message.Type.Text // 消息类型是否为文本
+    const isRoom = roomWhiteList.includes(roomName) && content.includes(`${botName}`) // 是否在群聊白名单内并且艾特了机器人
+    const isAlias = aliasWhiteList.includes(remarkName) || aliasWhiteList.includes(name) // 发消息的人是否在联系人白名单内
+    const isBotSelf = botName === remarkName || botName === name // 是否是机器人自己
+    // TODO 你们可以根据自己的需求修改这里的逻辑
+    if (isText && !isBotSelf) {
+        try {
+            // 区分群聊和私聊
+            if (isRoom && room) {
+                await room.say(await getReply(content.replace(`${botName}`, '')))
+                return
+            }
+            // 私人聊天，白名单内的直接发送
+            if (isAlias && !room) {
+                await contact.say(await getReply(content))
+            }
+        } catch (e) {
+            console.error(e)
+        }
     }
-  }
 }
 
 /**
@@ -46,29 +47,29 @@ export async function defaultMessage(msg, bot) {
  * @returns {Promise<void>}
  */
 export async function shardingMessage(message, bot) {
-  const talker = message.talker()
-  const isText = message.type() === bot.Message.Type.Text // 消息类型是否为文本
-  if (talker.self() || message.type() > 10 || (talker.name() === '微信团队' && isText)) {
-    return
-  }
-  const text = message.text()
-  const room = message.room()
-  if (!room) {
-    console.log(`Chat GPT Enabled User: ${talker.name()}`)
-    const response = await getChatGPTReply(text)
-    await trySay(talker, response)
-    return
-  }
-  let realText = splitMessage(text)
-  // 如果是群聊但不是指定艾特人那么就不进行发送消息
-  if (text.indexOf(`${botName}`) === -1) {
-    return
-  }
-  realText = text.replace(`${botName}`, '')
-  const topic = await room.topic()
-  const response = await getChatGPTReply(realText)
-  const result = `${realText}\n ---------------- \n ${response}`
-  await trySay(room, result)
+    const talker = message.talker()
+    const isText = message.type() === bot.Message.Type.Text // 消息类型是否为文本
+    if (talker.self() || message.type() > 10 || (talker.name() === '微信团队' && isText)) {
+        return
+    }
+    const text = message.text()
+    const room = message.room()
+    if (!room) {
+        console.log(`Chat GPT Enabled User: ${talker.name()}`)
+        const response = await getChatGPTReply(text)
+        await trySay(talker, response)
+        return
+    }
+    let realText = splitMessage(text)
+    // 如果是群聊但不是指定艾特人那么就不进行发送消息
+    if (text.indexOf(`${botName}`) === -1) {
+        return
+    }
+    realText = text.replace(`${botName}`, '')
+    const topic = await room.topic()
+    const response = await getChatGPTReply(realText)
+    const result = `${realText}\n ---------------- \n ${response}`
+    await trySay(room, result)
 }
 
 // 分片长度
@@ -81,16 +82,16 @@ const SINGLE_MESSAGE_MAX_SIZE = 500
  * @returns {Promise<void>}
  */
 async function trySay(talker, msg) {
-  const messages = []
-  let message = msg
-  while (message.length > SINGLE_MESSAGE_MAX_SIZE) {
-    messages.push(message.slice(0, SINGLE_MESSAGE_MAX_SIZE))
-    message = message.slice(SINGLE_MESSAGE_MAX_SIZE)
-  }
-  messages.push(message)
-  for (const msg of messages) {
-    await talker.say(msg)
-  }
+    const messages = []
+    let message = msg
+    while (message.length > SINGLE_MESSAGE_MAX_SIZE) {
+        messages.push(message.slice(0, SINGLE_MESSAGE_MAX_SIZE))
+        message = message.slice(SINGLE_MESSAGE_MAX_SIZE)
+    }
+    messages.push(message)
+    for (const msg of messages) {
+        await talker.say(msg)
+    }
 }
 
 /**
@@ -99,10 +100,10 @@ async function trySay(talker, msg) {
  * @returns {Promise<*>}
  */
 async function splitMessage(text) {
-  let realText = text
-  const item = text.split('- - - - - - - - - - - - - - -')
-  if (item.length > 1) {
-    realText = item[item.length - 1]
-  }
-  return realText
+    let realText = text
+    const item = text.split('- - - - - - - - - - - - - - -')
+    if (item.length > 1) {
+        realText = item[item.length - 1]
+    }
+    return realText
 }
